7ecea0a1067016b046397f5dbc3f017c03a616b1
username <user@email.com>
2023-03-30 12:49:50 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace TreesLubo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btn_nameri = new System.Windows.Forms.Button();
+            this.txt_naimenovanie = new System.Windows.Forms.TextBox();
+            this.cmb_Otdel = new System.Windows.Forms.ComboBox();
+            this.cmb_rod = new System.Windows.Forms.ComboBox();
+            this.cmb_vid = new System.Windows.Forms.ComboBox();
+            this.cmb_semeistvo = new System.Windows.Forms.ComboBox();
+            this.cmb_razred = new System.Windows.Forms.ComboBox();
+            this.comboBox6 = new System.Windows.Forms.ComboBox();
+            this.lbl_naimenovanie = new System.Windows.Forms.Label();
+            this.lbl_opisanie = new System.Windows.Forms.Label();
+            this.txt_opisanie = new System.Windows.Forms.TextBox();
+            this.lbl_otdel = new System.Windows.Forms.Label();
+            this.lbl_klas = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_nameri
+            // 
+            this.btn_nameri.Location = new System.Drawing.Point(72, 420);
+            this.btn_nameri.Name = "btn_nameri";
+            this.btn_nameri.Size = new System.Drawing.Size(391, 66);
+            this.btn_nameri.TabIndex = 0;
+            this.btn_nameri.Text = "ОТКРИЙ ДЪРВОТО";
+            this.btn_nameri.UseVisualStyleBackColor = true;
+            // 
+            // txt_naimenovanie
+            // 
+            this.txt_naimenovanie.Location = new System.Drawing.Point(72, 46);
+            this.txt_naimenovanie.Multiline = true;
+            this.txt_naimenovanie.Name = "txt_naimenovanie";
+            this.txt_naimenovanie.Size = new System.Drawing.Size(391, 37);
+            this.txt_naimenovanie.TabIndex = 1;
+            // 
+            // cmb_Otdel
+            // 
+            this.cmb_Otdel.FormattingEnabled = true;
+            this.cmb_Otdel.Location = new System.Drawing.Point(72, 151);
+            this.cmb_Otdel.Name = "cmb_Otdel";
+            this.cmb_Otdel.Size = new System.Drawing.Size(142, 24);
+            this.cmb_Otdel.TabIndex = 2;
+            // 
+            // cmb_rod
+            // 
+            this.cmb_rod.FormattingEnabled = true;
+            this.cmb_rod.Location = new System.Drawing.Point(72, 316);
+            this.cmb_rod.Name = "cmb_rod";
+            this.cmb_rod.Size = new System.Drawing.Size(142, 24);
+            this.cmb_rod.TabIndex = 3;
+            // 
+            // cmb_vid
+            // 
+            this.cmb_vid.FormattingEnabled = true;
+            this.cmb_vid.Location = new System.Drawing.Point(321, 316);
+            this.cmb_vid.Name = "cmb_vid";
+            this.cmb_vid.Size = new System.Drawing.Size(142, 24);
+            this.cmb_vid.TabIndex = 4;
+            // 
+            // cmb_semeistvo
+            // 
+            this.cmb_semeistvo.FormattingEnabled = true;
+            this.cmb_semeistvo.Location = new System.Drawing.Point(321, 225);
+            this.cmb_semeistvo.Name = "cmb_semeistvo";
+            this.cmb_semeistvo.Size = new System.Drawing.Size(142, 24);
+            this.cmb_semeistvo.TabIndex = 5;
+            // 
+            // cmb_razred
+            // 
+            this.cmb_razred.FormattingEnabled = true;
+            this.cmb_razred.Location = new System.Drawing.Point(72, 225);
+            this.cmb_razred.Name = "cmb_razred";
+            this.cmb_razred.Size = new System.Drawing.Size(142, 24);
+            this.cmb_razred.TabIndex = 6;
+            // 
+            // comboBox6
+            // 
+            this.comboBox6.FormattingEnabled = true;
+            this.comboBox6.Location = new System.Drawing.Point(321, 151);
+            this.comboBox6.Name = "comboBox6";
+            this.comboBox6.Size = new System.Drawing.Size(142, 24);
+            this.comboBox6.TabIndex = 7;
+            // 
+            // lbl_naimenovanie
+            // 
+            this.lbl_naimenovanie.AutoSize = true;
+            this.lbl_naimenovanie.Location = new System.Drawing.Point(200, 9);
+            this.lbl_naimenovanie.Name = "lbl_naimenovanie";
+            this.lbl_naimenovanie.Size = new System.Drawing.Size(127, 16);
+            this.lbl_naimenovanie.TabIndex = 8;
+            this.lbl_naimenovanie.Text = "НАЙМЕНОВАНИЕ";
+            // 
+            // lbl_opisanie
+            // 
+            this.lbl_opisanie.AutoSize = true;
+            this.lbl_opisanie.Location = new System.Drawing.Point(788, 49);
+            this.lbl_opisanie.Name = "lbl_opisanie";
+            this.lbl_opisanie.Size = new System.Drawing.Size(86, 16);
+            this.lbl_opisanie.TabIndex = 9;
+            this.lbl_opisanie.Text = "ОПИСАНИЕ";
+            // 
+            // txt_opisanie
+            // 
+            this.txt_opisanie.Location = new System.Drawing.Point(652, 95);
+            this.txt_opisanie.Multiline = true;
+            this.txt_opisanie.Name = "txt_opisanie";
+            this.txt_opisanie.Size = new System.Drawing.Size(391, 410);
+            this.txt_opisanie.TabIndex = 10;
+            // 
+            // lbl_otdel
+            // 
+            this.lbl_otdel.AutoSize = true;
+            this.lbl_otdel.Location = new System.Drawing.Point(115, 132);
+            this.lbl_otdel.Name = "lbl_otdel";
+            this.lbl_otdel.Size = new System.Drawing.Size(48, 16);
+            this.lbl_otdel.TabIndex = 11;
+            this.lbl_otdel.Text = "Отдел";
+            // 
+            // lbl_klas
+            // 
+            this.lbl_klas.AutoSize = true;
+            this.lbl_klas.Location = new System.Drawing.Point(366, 132);
+            this.lbl_klas.Name = "lbl_klas";
+            this.lbl_klas.Size = new System.Drawing.Size(38, 16);
+            this.lbl_klas.TabIndex = 12;
+            this.lbl_klas.Text = "Клас";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(115, 206);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 16);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Разред";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(349, 206);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 16);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Семейство";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(115, 297);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 16);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Род";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(366, 297);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 16);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Вид";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1211, 571);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbl_klas);
+            this.Controls.Add(this.lbl_otdel);
+            this.Controls.Add(this.txt_opisanie);
+            this.Controls.Add(this.lbl_opisanie);
+            this.Controls.Add(this.lbl_naimenovanie);
+            this.Controls.Add(this.comboBox6);
+            this.Controls.Add(this.cmb_razred);
+            this.Controls.Add(this.cmb_semeistvo);
+            this.Controls.Add(this.cmb_vid);
+            this.Controls.Add(this.cmb_rod);
+            this.Controls.Add(this.cmb_Otdel);
+            this.Controls.Add(this.txt_naimenovanie);
+            this.Controls.Add(this.btn_nameri);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_nameri;
+        private System.Windows.Forms.TextBox txt_naimenovanie;
+        private System.Windows.Forms.ComboBox cmb_Otdel;
+        private System.Windows.Forms.ComboBox cmb_rod;
+        private System.Windows.Forms.ComboBox cmb_vid;
+        private System.Windows.Forms.ComboBox cmb_semeistvo;
+        private System.Windows.Forms.ComboBox cmb_razred;
+        private System.Windows.Forms.ComboBox comboBox6;
+        private System.Windows.Forms.Label lbl_naimenovanie;
+        private System.Windows.Forms.Label lbl_opisanie;
+        private System.Windows.Forms.TextBox txt_opisanie;
+        private System.Windows.Forms.Label lbl_otdel;
+        private System.Windows.Forms.Label lbl_klas;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,34 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TreesLubo
+{
+    public partial class Form1 : Form
+    {
+        string connstr = "server=192.168.1.237;" + "port=3306;" + 
+            "user=REMOTE;" + "password=Aa123456@;" +
+            "database=trees";
+        MySqlConnection connect;
+        MySqlCommand query;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            connect = new MySqlConnection(connstr);
+            if (connect.State == 0) connect.Open();
+            MessageBox.Show("Connection now oppening");
+        }
+    }
+}
